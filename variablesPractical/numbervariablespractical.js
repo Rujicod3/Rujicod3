fd5d4f936c6454e68b9ee28884d754d32f195c6c
Ronny <Ronaldmoore30@gmail.com>
2024-10-02 13:25:55 -0500

    Add files via upload

---
+++ variablesPractical/numbervariablespractical.js
@@ -0,0 +1,42 @@
+console.log('jsnm');
+
+let motorcycles = 9;
+let cars = 0;
+let vehicles = motorcycles + cars;
+console.log('bikes:', motorcycles);
+console.log( 'cars:', cars);
+console.log('total vehicles:', vehicles);
+
+console.log('had to put the sof tail in the garage' );
+
+// decrement motorcycles
+motorcycles--; // subtract 1 from variable
+console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars);
+vehicles = motorcycles + cars;
+console.log('total vehicles:', vehicles);
+
+
+console.log('missed the bike so I bought 2 lambos');
+cars += 2;
+console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars);
+vehicles = motorcycles + cars;
+console.log('total vehicles:', vehicles);
+console.log( 'wanted a jeep, so I got one');
+cars++;
+vehicles = motorcycles + cars;
+console.log('total vehicles:', vehicles);
+console.log('sold 3 bikes');
+motorcycles -= 3;
+console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars);
+vehicles = motorcycles + cars;
+console.log('total vehicles:', vehicles);
+
+console.log('got the soft tail back from garage, is now moded');
+motorcycles++;
+console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars);
+vehicles = motorcycles + cars;
+console.log('total vehicles:', vehicles);
